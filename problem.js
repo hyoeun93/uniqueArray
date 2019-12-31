ef241d295a13fc39eacb22da9feede894cfad47e
HyoEun Lee <hyoeun93@gmail.com>
2019-12-30 20:47:45 -0500

    solution

--- problem.js
+++ problem.js
@@ -5,7 +5,20 @@ takes an array and gives back an array where all the items are unique
 
 let array = [1, 2, 2, 3];
 
-console.log(
-    uniqueArray1(array)
-);
+function uniqueArray(arr) {
+    let seen = {};
+    let out = [];
+    for(let i = 0; i < arr.elngth; i++) {
+        let el = a[i];
 
+        if(!(el in seen)) {
+            seen[el] = true;
+            out.push(el);
+        }
+    }
+    return out;
+}
+
+console.log(
+    uniqueArray(array)
+);
