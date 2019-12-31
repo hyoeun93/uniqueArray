190140c689e2c26da5c404f495f4ab61dd0a98ff
HyoEun Lee <hyoeun93@gmail.com>
2019-12-31 17:12:05 -0500

    solution2

--- problem.js
+++ problem.js
@@ -5,6 +5,7 @@ takes an array and gives back an array where all the items are unique
 
 let array = [1, 2, 2, 3];
 
+//solution 1
 function uniqueArray(arr) {
     let seen = {};
     let out = [];
@@ -19,6 +20,13 @@ function uniqueArray(arr) {
     return out;
 }
 
+//solution 2
+let uniqueArray2 = (a) => {
+    let seen = {};
+    return a.filter((el) => seen.hasOwnProperty(el) ? false: seen[el] = true);
+}
+
 console.log(
-    uniqueArray(array)
+    uniqueArray(array),
+    uniqueArray2(array)
 );
